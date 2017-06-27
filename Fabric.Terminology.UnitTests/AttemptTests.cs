075a7accac5aed3c5e50bfee8e45af53c1078f2e
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-06-27 08:28:32 -0700

    Tests for Attempt<T>

---
+++ Fabric.Terminology.UnitTests/AttemptTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Fabric.Terminology.UnitTests
+{
+    using System;
+
+    using Fabric.Terminology.Domain;
+    using Fabric.Terminology.TestsBase.Mocks;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class AttemptTests
+    {
+        [Fact]
+        public void CanReturnSuccessfulAttempt()
+        {
+            // Arrange
+            var dt = DateTime.Today;
+            var obj = new TestObject { Text = "attempt", Stamp = dt };
+
+            // Act
+            var attempt = Attempt<TestObject>.Successful(obj);
+
+            // Assert
+            attempt.Success.Should().BeTrue();
+            attempt.Exception.Should().BeNull();
+            attempt.Result.ShouldBeEquivalentTo(obj);
+        }
+
+        [Fact]
+        public void CanReturnFailedAttemptWithoutResult()
+        {
+            // Arrange
+            var argEx = new ArgumentException("test");
+
+            // Act
+            var attempt = Attempt<TestObject>.Failed(argEx);
+
+            // Assert
+            attempt.Success.Should().BeFalse();
+            attempt.Exception.Should().NotBeNull();
+            attempt.Exception.ShouldBeEquivalentTo(argEx);
+            attempt.Result.Should().BeNull();
+        }
+
+        [Fact]
+        public void CanReturnFailedAttemptWithResult()
+        {
+            // Arrange
+            var dt = DateTime.Today;
+            var obj = new TestObject { Text = "attempt", Stamp = dt };
+            var argEx = new ArgumentException("test");
+
+            // Act
+            var attempt = Attempt<TestObject>.Failed(argEx, obj);
+
+            // Assert
+            attempt.Success.Should().BeFalse();
+            attempt.Exception.Should().NotBeNull();
+            attempt.Result.Should().NotBeNull();
+            attempt.Exception.ShouldBeEquivalentTo(argEx);
+            attempt.Result.ShouldBeEquivalentTo(obj);
+        }
+    }
+}
