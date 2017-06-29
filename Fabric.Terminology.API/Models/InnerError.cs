37aa6a6f548949221b5b331df74626db4dda3e01
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-06-29 10:43:46 -0700

    Splits errors into two files per standard guidelines.

--- Fabric.Terminology.API/Models/InnerError.cs
+++ Fabric.Terminology.API/Models/InnerError.cs
@@ -1,15 +1,9 @@
 ﻿namespace Fabric.Terminology.API.Models
 {
     // acquired from Fabric.Authorization.Domain
-    public class Error
+    public class InnerError
     {
         public string Code { get; set; }
-
-        public string Message { get; set; }
-
-        public string Target { get; set; }
-
-        public Error[] Details { get; set; }
 
         public InnerError Innererror { get; set; }
     }
