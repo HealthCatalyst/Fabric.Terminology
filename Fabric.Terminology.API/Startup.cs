32b278fc2079cf765c6e594f2eff7ba0b902a12a
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-07-27 14:08:34 -0700

    Cleanup code formatting.

--- Fabric.Terminology.API/Startup.cs
+++ Fabric.Terminology.API/Startup.cs
@@ -12,7 +12,6 @@
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
     using Nancy.Owin;
