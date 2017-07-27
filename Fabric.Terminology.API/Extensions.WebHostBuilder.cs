a0ab5c205aa805e7d0413530adb08c92186be779
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-07-27 14:49:17 -0700

    Wires webhost configuration extension in startup.

--- Fabric.Terminology.API/Extensions.WebHostBuilder.cs
+++ Fabric.Terminology.API/Extensions.WebHostBuilder.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Fabric.Terminology.API
+﻿namespace Fabric.Terminology.API
 {
     using Fabric.Terminology.API.Configuration;
 
