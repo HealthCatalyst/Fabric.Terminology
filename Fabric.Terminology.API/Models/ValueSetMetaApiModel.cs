249bad9ba20d368ad01e00cfb5649858a60fa1e2
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-07-31 13:48:28 -0700

    Introduces the ValueSetMetaApiModel

---
+++ Fabric.Terminology.API/Models/ValueSetMetaApiModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Fabric.Terminology.API.Models
+{
+    public class ValueSetMetaApiModel
+    {
+        public string AuthoringSourceDescription { get; set; }
+
+        public string PurposeDescription { get; set; }
+
+        public string SourceDescription { get; set; }
+
+        public string VersionDescription { get; set; }
+    }
+}
