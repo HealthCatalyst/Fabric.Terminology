0f377aa6e47ca52250815070380fc71157c76ff2
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-08-15 13:07:58 -0600

    Terminology team meeting TODO notes.

--- Fabric.Terminology.Domain/Models/ICodeSetCode.cs
+++ Fabric.Terminology.Domain/Models/ICodeSetCode.cs
@@ -8,14 +8,18 @@ namespace Fabric.Terminology.Domain.Models
 
         string Name { get; }
 
+        // TODO REMOVE
         string VersionDescription { get; }
 
+        // TODO REMOVE
         string SourceDescription { get; }
 
+        // TODO REMOVE
         DateTime? RevisionDate { get; }
 
         ICodeSystem CodeSystem { get; }
 
+        // TODO REMOVE
         DateTime LastLoadDate { get; }
     }
 }
--- Fabric.Terminology.Domain/Models/IValueSetMeta.cs
+++ Fabric.Terminology.Domain/Models/IValueSetMeta.cs
@@ -1,13 +1,21 @@
 ﻿namespace Fabric.Terminology.Domain.Models
 {
+    using System;
+
     public interface IValueSetMeta
     {
+        // TODO AuthorDSC
         string AuthoringSourceDescription { get; }
 
+        // TODO REMOVE
         string PurposeDescription { get; }
 
         string SourceDescription { get; }
 
+        // TODO REmove
         string VersionDescription { get; }
+
+        // TODO ADD
+        // DateTime VersionDTS { get; }
     }
 }
