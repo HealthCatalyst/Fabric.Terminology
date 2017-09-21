823c5a0ab2e64c693a636bd4ecfdbebcb4297065
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-09-21 16:55:58 -0700

    Renames mappers to factories per pattern.

---
+++ Fabric.Terminology.SqlServer/Persistence/Factories/ValueSetCodeCountFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Fabric.Terminology.SqlServer.Persistence.Mapping
+{
+    using Fabric.Terminology.Domain.Models;
+    using Fabric.Terminology.Domain.Persistence.Factories;
+    using Fabric.Terminology.SqlServer.Models.Dto;
+
+    internal sealed class ValueSetCodeCountFactory : IModelFactory<ValueSetCodeCountDto, IValueSetCodeCount>
+    {
+        public IValueSetCodeCount Build(ValueSetCodeCountDto dto)
+        {
+            return new ValueSetCodeCount
+            {
+                CodeCount = dto.CodeSystemPerValueSetNBR,
+                CodeSystemGuid = dto.CodeSystemGUID,
+                ValueSetGuid = dto.ValueSetGUID
+            };
+        }
+    }
+}
