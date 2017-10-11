8a8c3110aa1aab50f32e9174577c48f583099400
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-10-11 16:15:02 -0700

    Refactors /valuesets/ endpoint to expect a single valueSetGuid (changed from optional CSV).  Introduces /valuesets/multiple/ POST which accepts an array of valueSetGuid(s).

---
+++ Fabric.Terminology.API/Models/MultipleValueSetsQuery.cs
@@ -0,0 +1,15 @@
+﻿namespace Fabric.Terminology.API.Models
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    public class MultipleValueSetsQuery : MultipleCodeSystemQuery
+    {
+        [JsonProperty("valueSetGuids")]
+        public Guid[] ValueSetGuids { get; set; }
+
+        [JsonProperty("summary")]
+        public bool Summary { get; set; } = true;
+    }
+}
