b9e69dc886831ff14ed11a15a7134fe83f97723d
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-10-20 14:26:54 -0700

    [WIP] Introducing IValueSetCodeService.

---
+++ Fabric.Terminology.Domain/Services/IValueSetCodeService.cs
@@ -0,0 +1,34 @@
+﻿namespace Fabric.Terminology.Domain.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Fabric.Terminology.Domain.Models;
+
+    public interface IValueSetCodeService
+    {
+        IReadOnlyCollection<IValueSetCode> GetValueSetCodes(Guid valueSetGuid);
+
+        /// <summary>
+        ///     Get a collection of value set codes by the codeGuid.
+        /// </summary>
+        /// <remarks>
+        ///     Useful for determining which value sets to which a <see cref="ICodeSystemCode" /> is associated.
+        /// </remarks>
+        IReadOnlyCollection<IValueSetCode> GetValueSetCodesByCodeGuid(Guid codeGuid);
+
+        Task<PagedCollection<IValueSetCode>> GetValueSetCodesAsync(Guid valueSetGuid, IPagerSettings settings);
+
+        Task<PagedCollection<IValueSetCode>> GetValueSetCodesAsync(
+            Guid valueSetGuid,
+            IPagerSettings settings,
+            IEnumerable<Guid> codeSystemGuids);
+
+        Task<PagedCollection<IValueSetCode>> GetValueSetCodesAsync(
+            string filterText,
+            Guid valueSetGuid,
+            IPagerSettings settings,
+            IEnumerable<Guid> codeSystemGuids);
+    }
+}
---
+++ Fabric.Terminology.SqlServer/Services/SqlValueSetCodeService.cs
@@ -0,0 +1,57 @@
+﻿namespace Fabric.Terminology.SqlServer.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Fabric.Terminology.Domain.Models;
+    using Fabric.Terminology.Domain.Services;
+    using Fabric.Terminology.SqlServer.Persistence;
+
+    using Serilog;
+
+    internal class SqlValueSetCodeService : IValueSetCodeService
+    {
+        private readonly ILogger logger;
+
+        private readonly IValueSetCodeRepository valueSetCodeRepository;
+
+        public SqlValueSetCodeService(ILogger logger, IValueSetCodeRepository valueSetCodeRepository)
+        {
+            this.logger = logger;
+            this.valueSetCodeRepository = valueSetCodeRepository;
+        }
+
+        public IReadOnlyCollection<IValueSetCode> GetValueSetCodes(Guid valueSetGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<IValueSetCode> GetValueSetCodesByCodeGuid(Guid codeGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedCollection<IValueSetCode>> GetValueSetCodesAsync(Guid valueSetGuid, IPagerSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedCollection<IValueSetCode>> GetValueSetCodesAsync(
+            Guid valueSetGuid,
+            IPagerSettings settings,
+            IEnumerable<Guid> codeSystemGuids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedCollection<IValueSetCode>> GetValueSetCodesAsync(
+            string filterText,
+            Guid valueSetGuid,
+            IPagerSettings settings,
+            IEnumerable<Guid> codeSystemGuids)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
