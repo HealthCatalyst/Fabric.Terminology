f82e706c3ef663cae191579144dc7dd56b1b0544
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2018-07-23 16:01:42 -0700

    Removes the GenerateAssemblyInfo attribute from projects so that version is applied from csproj.  Adds unit test to assert version conforms to version.json.

---
+++ Fabric.Terminology.TestsBase/Models/VersionConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Fabric.Terminology.TestsBase.Models
+{
+    public class VersionConfig
+    {
+        public string Version { get; set; }
+
+        public string ReleaseComment { get; set; }
+    }
+}
