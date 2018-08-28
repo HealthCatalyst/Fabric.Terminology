2c240de919b90ac28d1c5447a82047f043a11244
Ben Anderson <ben.anderson@healthcatalyst.com>
2018-08-28 16:08:18 -0600

    added database dacpac project

---
+++ Fabric.Terminology.Database/Storage/HCFabricTerminologyData1.sql
@@ -0,0 +1,22 @@
+/*
+Do not change the database path or name variables.
+Any sqlcmd variables will be properly substituted during 
+build and deployment.
+*/
+
+ALTER DATABASE [$(DatabaseName)]
+	ADD FILEGROUP [HCFabricTerminologyData1] 
+
+	GO
+ALTER DATABASE [$(DatabaseName)]
+	ADD FILE
+	(
+		NAME = [HCFabricTerminologyData1File1],
+		FILENAME = '$(FabricTerminologyDataMountPoint)\HC$(DatabaseName)Data1File1.ndf',
+		SIZE = 100MB,
+		MAXSIZE = 5GB,
+		FILEGROWTH = 100MB
+	)
+
+TO FILEGROUP [HCFabricTerminologyData1];
+GO
