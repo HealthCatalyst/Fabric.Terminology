f440c117f00540d54b1083794a2101e60ea65b1e
Ben Anderson <ben.anderson@healthcatalyst.com>
2018-08-29 16:03:48 -0600

    now use DosInstallUtilties when deploying dacpac

--- Fabric.Terminology.API/Scripts/Terminology-Intall-Utilities.psm1
+++ Fabric.Terminology.API/Scripts/Terminology-Intall-Utilities.psm1
@@ -152,10 +152,9 @@ function Publish-TerminologyDatabaseUpdates() {
         [string] $Dacpac,
         [String] $PublishProfile
     ) 
-    Import-Module dbatools
-
-    Publish-DbaDacpac -SqlInstance $config.sqlAddress -Database "Terminology" -PublishXml $PublishProfile -Path $Dacpac
-    #Publish-DosDacPac -TargetSqlInstance $config.sqlAddress -DacPacFilePath $Dacpac -TargetDb "Terminology" -PublishOptionsFilePath $PublishProfile
+    Write-DosMessage -Level "Information" -Message "Creating or updating Terminology database on $($config.sqlAddress)"
+    
+    Publish-DosDacPac -TargetSqlInstance $config.sqlAddress -DacPacFilePath $Dacpac -TargetDb "Terminology" -PublishOptionsFilePath $PublishProfile
 
     <# TODO: Ben create terminology db through dacpac
     - add the following to Fabric.Identity.InstallPackage.targets: https://github.com/HealthCatalyst/Fabric.Identity/blob/master/Fabric.Identity.API/scripts/Fabric.Identity.InstallPackage.targets
